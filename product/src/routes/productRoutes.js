b3bb52652369b6a8e2faa867e36d033778f63e26
Tran Trung <tranvantrung809@gmail.com>
2025-11-03 01:42:33 +0700

    asdasdas

--- product/src/routes/productRoutes.js
+++ product/src/routes/productRoutes.js
@@ -8,9 +8,6 @@ const productController = new ProductController();
 router.post("/", isAuthenticated, productController.createProduct);
 router.post("/buy", isAuthenticated, productController.createOrder);
 router.get("/", isAuthenticated, productController.getProducts);
-<<<<<<< HEAD
 router.get("/:id", isAuthenticated, productController.getProductById);
-=======
 
->>>>>>> 72a0403eaeb1263f547bc321085b38ea757eb449
 module.exports = router;
