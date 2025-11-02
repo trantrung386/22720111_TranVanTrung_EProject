c8b8179fc2a32bfe78b02d832e301d16a480878e
Tran Trung <tranvantrung809@gmail.com>
2025-11-03 02:04:53 +0700

    asdasdasd

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -109,27 +109,6 @@ class ProductController {
     }
   }
 
-  async getProductById(req, res, next){
-    try{
-      const token = req.headers.authorization;
-      if(!token){
-        return res.status(401).json({message : "Unauthorized"});
-      }
-      const {id} = req.params;
-      const product = await Product.findById(id);
-      if(!product){
-        // SỬA LỖI Ở ĐÂY: Thêm 404
-        return res.status(404).json({message : "Product not found"}); 
-      } 
-      res.status(200).json(product);
-
-    }
-    catch (error) {
-      console.error("Error in getProductById:", error);
-      res.status(500).json({ message: "Server error" });
-    }
-
-  } 
 }
   
 
