35e22f9601bb19bb3a6491b3223a404a2e67632b
Tran Trung <tranvantrung809@gmail.com>
2025-11-03 14:29:14 +0700

    ahhah

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -110,7 +110,7 @@ class ProductController {
   }
   async getProductByID(req, res, next){
     try{
-      const token = req.headfers.authorization;
+      const token = req.headers.authorization;
       if(!token){
         return res.status(401).json({message :"unautho"});
       }
